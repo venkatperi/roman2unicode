8d44e10ebc32468d90d524cd84111adbda625a06
Venkat Peri <venkatperi@gmail.com>
2020-10-17 07:52:34 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Input, Select } from "antd";
+import { Row, Col } from "antd";
+import "antd/dist/antd.css";
+import "./App.css";
+import { roman2unicode, UNICODE_BLOCKS } from "./utils/transliterate";
 
-function App() {
+const { TextArea } = Input;
+const { Option } = Select;
+
+const App = () => {
+  const [roman, setRoman] = useState<string>("");
+  const [target, setTarget] = useState(UNICODE_BLOCKS.telugu);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row>
+        <Col span={12}>Roman</Col>
+        <Col span={12}>
+          <Select
+            defaultValue={UNICODE_BLOCKS.telugu}
+            style={{ width: 120 }}
+            onChange={(x) => setTarget(x)}
+          >
+            <Option value={UNICODE_BLOCKS.devanagari}>Devanagari</Option>
+            <Option value={UNICODE_BLOCKS.telugu}>Telugu</Option>
+          </Select>
+        </Col>
+      </Row>
+      <Row className="height100">
+        <Col span={12} className="height100">
+          <TextArea
+            className="height100"
+            onChange={(x) => setRoman(x.target.value)}
+          />
+        </Col>
+        <Col span={12}>
+          <TextArea
+            className="height100"
+            readOnly
+            value={roman2unicode(roman, target)}
+          />
+        </Col>
+      </Row>
+    </>
   );
-}
+};
 
 export default App;
